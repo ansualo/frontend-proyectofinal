56cf4852282feeb5e964e1ccf76554dd6a38956b
Andrea <andrea.sualo@gmail.com>
2023-07-20 17:55:34 +0200

    style: add basic style to Profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,14 +1,37 @@
 import React from "react";
+import './Profile.css';
 import { useSelector } from "react-redux";
 import { usersData } from "../userSlice";
+import { Col, Container, Row } from "react-bootstrap";
 
 export const Profile = () => {
 
-    const datos = useSelector(usersData)
+    const data = useSelector(usersData)
 
-    console.log(datos)
+    console.log(data)
 
     return (
-        <div></div>
+        <div className="profileDesign">
+            <Container className="userProfile">
+                <Row className="profileRow">
+                    <Col xs={12} md={12}>
+                        <div className="profileLabel">Name</div>
+                        <div className="profileData">{data.data.name}</div>
+                    </Col>
+                    <Col sm={12} md={12}>
+                        <div className="profileLabel">Surname</div>
+                        <div className="profileData">{data.data.surname}</div>
+                    </Col>
+                    <Col sm={12} md={12}>
+                        <div className="profileLabel">City</div>
+                        <div className="profileData">{data.data.city}</div>
+                    </Col>
+                    <Col sm={12} md={12}>
+                        <div className="profileLabel">Country</div>
+                        <div className="profileData">{data.data.country}</div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     )
 }
