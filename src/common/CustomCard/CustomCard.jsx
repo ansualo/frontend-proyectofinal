ac35d33e9ea55d177ee07c30150f523fd257ce15
Andrea <andrea.sualo@gmail.com>
2023-07-20 11:43:47 +0200

    feat: add water and sun  icons to CustomCard

--- src/common/CustomCard/CustomCard.jsx
+++ src/common/CustomCard/CustomCard.jsx
@@ -2,17 +2,25 @@ import React from 'react';
 import './CustomCard.css';
 import Card from 'react-bootstrap/Card';
 import image from '../../assets/images/login.jpg'
+import waterIcon from '../../assets/images/water.png'
+import sunIcon from '../../assets/images/sun.png'
 
 export const CustomCard = ({ common_name, sunlight, watering }) => {
 
   return (
     <Card className="bg-dark text-white cardDesign">
-    <Card.Img src={image} alt="Card image" />
-    <Card.ImgOverlay>
-      <Card.Title className="text-dark">{common_name}</Card.Title>
-    </Card.ImgOverlay>
-        <Card.Text className="mt-2 mb-0">{sunlight}</Card.Text>
+      <Card.Img src={image} alt="Card image" />
+      <Card.ImgOverlay>
+        <Card.Title className="text-dark">{common_name}</Card.Title>
+      </Card.ImgOverlay>
+      <div className="d-flex justify-content-center">
+        <Card.Img className="plantIcon mt-2" src={sunIcon} alt="Card image" />
+        <Card.Text className="mb-0 mt-2">{sunlight}</Card.Text>
+      </div>
+      <div className="d-flex justify-content-center">
+        <Card.Img className="plantIcon" src={waterIcon} alt="Card image" />
         <Card.Text>{watering}</Card.Text>
-  </Card>
+      </div>
+    </Card>
   );
 }
