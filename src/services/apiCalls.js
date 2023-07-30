280289e4ce45a0a76c03beb0140f8a065e0b82bc
Andrea <andrea.sualo@gmail.com>
2023-07-30 14:20:59 +0200

    feat: add vercel.json

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -258,7 +258,7 @@ export const createPlant = async (newPlant, token) => {
 
 export const getCity = async (city) => {
 
-    let res = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=59811b2c2443aacedf1ccc7bb01cf180`)
+    let res = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=59811b2c2443aacedf1ccc7bb01cf180`)
     return res.data
 }
 
